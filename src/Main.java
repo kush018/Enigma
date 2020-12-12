bfef10cc9eeb82b44648fb241d234da12992795a
kush018 <64299526+kush018@users.noreply.github.com>
2020-12-12 15:36:44 +0530

    Made the program more user friendly

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     /*
     The main class
      */
 
-    public static void main(String[] args) {
-        Rotor rotor1 = new Rotor(12, "EKMFLGDQVZNTOWYHXUSPAIBRCJ");
-        Rotor rotor2 = new Rotor(23, "AJDKSIRUXBLHWTMCQGZNPYFVOE");
-        Rotor rotor3 = new Rotor(9, "BDFHJLCPRTXVZNYEIWGAKMUSQO");
-        RotorGroup group = new RotorGroup(new Rotor[] {rotor1, rotor2, rotor3});
-        Reflector reflector = new Reflector("ZYXWVUTSRQPONMLKJIHGFEDCBA");
-        PlugBoard plugBoard = new PlugBoard(new String[] {"AB", "CD", "EF", "GH", "IJ", "KL", "MN", "OP", "QR", "ST"});
-        Machine machine = new Machine(plugBoard, reflector, group);
-        String message = "PFXLOGXOXCISZ";
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter rotor names: ");
+        String rotorNames = reader.readLine();
+        System.out.print("Enter initial rotor settings: ");
+        String rotorSettingsStr = reader.readLine();
+        String[] rotorSettingsArrStr = rotorSettingsStr.split(" ");
+        int[] rotorSettingsArrInt = new int[rotorSettingsArrStr.length];
+        for (int i = 0; i < rotorSettingsArrInt.length; i++) {
+            rotorSettingsArrInt[i] = Integer.parseInt(rotorSettingsArrStr[i]);
+        }
+        RotorGroup rotorGroup = getRotorGroupByNames(rotorNames.split(" "), rotorSettingsArrInt);
+        System.out.print("Enter reflector name: ");
+        String reflectorName = reader.readLine();
+        Reflector reflector = getReflectorByName(reflectorName);
+        System.out.print("Enter plug board settings: ");
+        String plugBoardSettings = reader.readLine();
+        PlugBoard plugBoard = getPlugBoard(plugBoardSettings);
+        Machine machine = new Machine(plugBoard, reflector, rotorGroup);
+        System.out.print("Enter message: ");
+        String message = reader.readLine();
         String cipher = "";
         for (int i = 0; i < message.length(); i++) {
             cipher += machine.getChar(message.charAt(i));
             machine.advance();
         }
-        System.out.println(cipher);
+        System.out.println("Cipher: " + cipher);
     }
 
     public static RotorGroup getRotorGroupByNames(String[] rotorNames, int[] rotorSettings) {
@@ -32,5 +48,18 @@ public class Main {
             rotors[i] = new Rotor(rotorSettings[i], currentRotorWiring);
         }
         return new RotorGroup(rotors);
+    }
+
+    public static Reflector getReflectorByName(String name) {
+        for (int i = 0; i < WiringData.reflectorWiringData.length; i++) {
+            if (WiringData.reflectorWiringData[i][0].equals(name)) {
+                return new Reflector(WiringData.reflectorWiringData[i][1]);
+            }
+        }
+        return new Reflector("");
+    }
+
+    public static PlugBoard getPlugBoard(String plugBoardWiring) {
+        return new PlugBoard(plugBoardWiring.split(" "));
     }
 }
