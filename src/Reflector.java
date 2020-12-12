83d5d1f53f975c710ef70289ec417b3d4f641288
kush018 <64299526+kush018@users.noreply.github.com>
2020-12-12 10:13:50 +0530

    Added constructor to Reflector

--- src/Reflector.java
+++ src/Reflector.java
@@ -4,6 +4,17 @@ public class Reflector {
     //one dimensional int array that stores the positions the pins are wired to in sequential order
     private int[] wiring;
 
+    //constructor
+    public Reflector(String wiringInfo) {
+        //initialises the wiring array
+        wiring = new int[26];
+        //wiring info consists of all positions in the form of a compact String
+        //based on that information, we will form the int array wiring
+        for (int i = 0; i < 26; i++) {
+            wiring[i] = wiringInfo.charAt(i) - 65;
+        }
+    }
+
     //finds wired position of given pin
     public int getPosition(int pos) {
         return wiring[pos];
