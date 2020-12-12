d46bd0e3e4fefce9d755e423b3b4cf60fc4dea9d
kush018 <64299526+kush018@users.noreply.github.com>
2020-12-12 09:52:54 +0530

    Made Rotor.java and added a few methods

---
+++ src/Rotor.java
@@ -0,0 +1,59 @@
+public class Rotor {
+    /*
+    A class that represents a rotor in an enigma machine
+     */
+
+    //one dimensional int array that contains data about the wiring of the pins of the rotor for its "zero" setting
+    //the wiring is in sequential order i.e. the first int in the array tells which pin the first pin is wired to
+    private int[] wiring;
+
+    //int that stores the "setting" of the Rotor object (between 0 and 25)
+    private int setting;
+
+    //constructor for all Rotor objects
+    public Rotor(int setting, String wiringInfo) {
+        this.setting = setting;
+        //initialises the wiring array
+        wiring = new int[26];
+        //wiring info consists of all positions in the form of a compact String
+        //based on that information, we will form the int array wiring
+        for (int i = 0; i < 26; i++) {
+            wiring[i] = wiringInfo.charAt(i) - 65;
+        }
+    }
+
+    //method that advances the rotor's setting by one.
+    //must be called after each character gets translated by the machine
+    //returns - true - if the rotor to it's left needs to be advanced as well
+    //false - if the rotor to it's left need not be advanced by one step
+    public boolean advance() {
+        setting++;
+        if (setting == 26) {
+            setting = 0;
+            return true;
+        }
+        return false;
+    }
+
+    //returns which position pos is wired to
+    public int getWiredPosition(int pos) {
+        return wiring[pos];
+    }
+
+    //returns a position such that if that position is used as an argument
+    //for the method getWiredPosition(int pos) then it will return the argument entered
+    public int getWiredPositionInverse(int pos) {
+        return findInteger(pos, wiring);
+    }
+
+    //finds an integer in an unsorted one dimensional array of integers and returns the index of that integer
+    //if the integer does not exist, then it returns -1
+    private static int findInteger(int element, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == element) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
