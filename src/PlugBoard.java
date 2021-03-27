f366c1442960789eee677819f4e6b9e6bbf4aa8f
Kushal Galrani <kushal.galrani@gmail.com>
2021-03-27 18:54:48 +0530

    Documentation for plugboard class added

--- src/PlugBoard.java
+++ src/PlugBoard.java
@@ -1,10 +1,17 @@
+/**
+ * A class representing an enigma machine plugboard.
+ *
+ * @author Kushal Galrani
+ */
 public class PlugBoard {
-    //a class representing PlugBoards
 
     //a two dimensional array containing information about plug board wirings
     private final int[][] plugBoardWiring;
 
-    //constructor
+    /**
+     *
+     * @param connections A String array representation of the plugboard connections. Each element in this array represents a plugboard connection. Eg: If the letter A needs to be connected to F then an element "AF" should be there. Case does not matter. There may be upto 15 connections (elements)
+     */
     public PlugBoard(String[] connections) {
         plugBoardWiring = new int[connections.length][2];
         int i = 0;
@@ -15,6 +22,11 @@ public class PlugBoard {
         }
     }
 
+    /**
+     * Used to find the "position" that a given position is wired to in the plugboard
+     * @param pos The position whose wiring we need to find
+     * @return The position that is connected to
+     */
     public int findConnection(int pos) {
         for (int[] ints : plugBoardWiring) {
             if (ints[0] == pos) {
