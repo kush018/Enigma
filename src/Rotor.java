567a9ffdb4c24207706f0d483a397923e81e7169
Kushal Galrani <kushal.galrani@gmail.com>
2021-03-27 19:21:27 +0530

    Added documentation to the Rotor class

--- src/Rotor.java
+++ src/Rotor.java
@@ -1,7 +1,9 @@
+/**
+ * A class that represents a rotor in an enigma machine.
+ *
+ * @author Kushal Galrani
+ */
 public class Rotor {
-    /*
-    A class that represents a rotor in an enigma machine
-     */
 
     //one dimensional int array that contains data about the wiring of the pins of the rotor for its "zero" setting
     //the wiring is in sequential order i.e. the first int in the array tells which pin the first pin is wired to
@@ -10,7 +12,11 @@ public class Rotor {
     //int that stores the "setting" of the Rotor object (between 0 and 25)
     private int setting;
 
-    //constructor for all Rotor objects
+    /**
+     *
+     * @param setting The intial setting of the rotor
+     * @param wiringInfo The information regarding the wiring of the rotor in String representation. See WiringData class's code to understand how this String representation works
+     */
     public Rotor(int setting, String wiringInfo) {
         if (0 <= setting && setting <= 25) {
             this.setting = setting;
@@ -26,10 +32,11 @@ public class Rotor {
         }
     }
 
-    //method that advances the rotor's setting by one.
-    //must be called after each character gets translated by the machine
-    //returns - true - if the rotor to it's left needs to be advanced as well
-    //false - if the rotor to it's left need not be advanced by one step
+    /**
+     * Method that advances the rotor's setting by one.
+     * Must be called after each character gets encrypted/decrypted by the machine
+     * @return true if the rotor to it's left needs to be advanced as well
+     */
     public boolean advance() {
         setting++;
         if (setting == 26) {
@@ -39,7 +46,11 @@ public class Rotor {
         return false;
     }
 
-    //returns which position pos is wired to
+    /**
+     * Returns which position pos is wired to
+     * @param pos The position whose wiring needs to be found
+     * @return The position that it is wired to.
+     */
     public int getWiredPosition(int pos) {
         int wiredPos = wiring[(pos + setting) % 26] - setting;
         if (wiredPos < 0) {
@@ -49,8 +60,11 @@ public class Rotor {
         }
     }
 
-    //returns a position such that if that position is used as an argument
-    //for the method getWiredPosition(int pos) then it will return the argument entered
+    /**
+     * Is an "inverse function" of getWiredPosition
+     * @param pos The position whose wiring needs to be found.
+     * @return The position such that if it is used as an argument to getWiredPosition(), then it returns pos
+     */
     public int getWiredPositionInverse(int pos) {
         for (int i = 0; i < 26; i++) {
             if (getWiredPosition(i) == pos) {
@@ -60,10 +74,18 @@ public class Rotor {
         return 0;
     }
 
+    /**
+     *
+     * @return the current setting of the rotor
+     */
     public int getSetting() {
         return this.setting;
     }
 
+    /**
+     * Changes the setting of the rotor.
+     * @param setting The new setting
+     */
     public void setSetting(int setting) {
         if (0 <= setting && setting <= 25) {
             this.setting = setting;
